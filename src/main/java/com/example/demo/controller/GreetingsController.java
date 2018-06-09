f591cee4304174bebe6c446d67c110e82fac2c2f
Andrew <andrewyelmanov@gmail.com>
2018-06-10 01:46:14 +0300

    Замена на textarea Рыба для post запроса

--- src/main/java/com/example/demo/controller/GreetingsController.java
+++ src/main/java/com/example/demo/controller/GreetingsController.java
@@ -4,6 +4,7 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -15,4 +16,8 @@ public class GreetingsController {
         return "greeting";
     }
 
+    @PostMapping("/")
+    public String readCode(String code){
+        return "greeting";
+    }
 }
